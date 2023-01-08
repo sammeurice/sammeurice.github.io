d9c2f748e7cc92841d8046c47a9921e3ea2e5a40
Sam <sam.meurice@gmail.com>
2023-01-07 20:51:30 -0500

    Fixed Profile pics

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,10 @@
 import React from "react";
 import * as s from "./StyledComponents";
 import { AiFillStar } from "react-icons/ai";
-import BgImage from "../assets/review1.jpg";
 
 function UserWholeReview({ image, review, name }) {
   return (
-    <s.User style={{ backgroundImage: `url(${BgImage})` }}>
+    <s.User style={{ backgroundImage: `url(${image})` }}>
       <s.UserReview>
         <AiFillStar size={24} />
         <AiFillStar size={24} />
